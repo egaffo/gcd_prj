d2280ea8076d6d0f1c6735abffcad364d0b3da1a
Enrico Gaffo <enrico.gaffo@gmail.com>
2015-07-23 15:13:13 +0200

    use file paths for file name locations instead of full strings

--- run_analysis.R
+++ run_analysis.R
@@ -5,39 +5,51 @@
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average 
 #    of each variable for each activity and each subject.
 
+data_dir      <- file.path("UCI HAR Dataset")
+test_set.fp   <- file.path(data_dir, "test", "X_test.txt")
+train_set.fp  <- file.path(data_dir, "train", "X_train.txt")
+features.fp   <- file.path(data_dir, "features.txt")
+activities.fp <- file.path(data_dir, "activity_labels.txt")
+test_activity.fp <- file.path(data_dir, "test", "y_test.txt")
+train_activities.fp <- file.path(data_dir, "train", "y_train.txt")
+test_sbj.fp  <- file.path(data_dir, "test", "subject_test.txt")
+train_sbj.fp <- file.path(data_dir, "train", "subject_train.txt")
+
 library(dplyr)
 
 ## test
-test_set <- read.table("UCI HAR Dataset/test/X_test.txt", header = F, colClasses = "numeric")
-
+test_set <- read.table(test_set.fp, header = F, colClasses = "numeric")
 ## train
-train_set <- read.table("UCI HAR Dataset/train/X_train.txt", header = F, colClasses = "numeric")
+train_set <- read.table(train_set.fp, header = F, colClasses = "numeric")
 
 ## 1. merge sets and 2. extract mean and std
-features <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors = F,
-                       col.names = c("feature_id", "feature_description"))
-ms.features <- features[which(grepl(pattern = "mean|std", x = features$feature_description)),]
+features <- read.table(features.fp, stringsAsFactors = F, col.names = c("feature_id", "feature_description"))
+
+ms.features <- features[which(grepl(pattern = "mean\\(|std\\(", x = features$feature_description)),]
+
 full_set <- rbind(test_set, train_set)[, ms.features$feature_id]
+
 ## 4. label the data set with descriptive variable names
 colnames(full_set) <- ms.features$feature_description
+
 # 3. Use descriptive activity names
-activities <- read.table("UCI HAR Dataset/activity_labels.txt", 
-                         col.names = c("activity_id", "activity_description"))
-test_activity <- read.table("UCI HAR Dataset/test/y_test.txt", header = F, 
-                            colClasses = "numeric", col.names = c("activity_id"))
+activities <- read.table(activities.fp, col.names = c("activity_id", "activity_description"))
+
+test_activity <- read.table(test_activity.fp, header = F, colClasses = "numeric", col.names = c("activity_id"))
 test_activities <- merge(test_activity, activities, by = "activity_id", all.x = T, all.y = F)
 r.test_activities <- as.character(test_activities$activity_description)
-train_activity <- read.table("UCI HAR Dataset/train/y_train.txt", header = F, 
-                             colClasses = "numeric", col.names = c("activity_id"))
+
+train_activity <- read.table(train_activities.fp, header = F, colClasses = "numeric", col.names = c("activity_id"))
 train_activities <- merge(train_activity, activities, by = "activity_id", all.x = T, all.y = F)
 r.train_activities <- as.character(train_activities$activity_description)
 
-test_sbj <- scan("UCI HAR Dataset/test/subject_test.txt", what = "c")
-train_sbj <- scan("UCI HAR Dataset/train/subject_train.txt", what = "c")
+test_sbj <- scan(test_sbj.fp, what = "c")
+train_sbj <- scan(train_sbj.fp, what = "c")
+
 full_set <- mutate(full_set, activity = c(r.test_activities, r.train_activities),
                    subjects = c(test_sbj, train_sbj))
 
-## make tidy data
+## 5. make tidy data and write it
 tidy_ds <- group_by(full_set, activity, subjects) %>% summarise_each(funs(mean, "mean", mean(., na.rm = TRUE)))
 # tidy_ds.act <- group_by(full_set[, colnames(full_set)!="subjects"], 
 #                         activity) %>% summarise_each(funs(mean, "mean", mean(., na.rm = TRUE)))
