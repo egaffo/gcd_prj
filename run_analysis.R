4787b31ab7aef7e22f7d7109277fac9d72e14533
Enrico Gaffo <enrico.gaffo@gmail.com>
2015-07-23 17:13:17 +0200

    added data file download. Merge test and train stuff with ldply. Follow assignments steps order

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,12 @@
-# 1. Merges the training and the test sets to create one data set.
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-# 3. Uses descriptive activity names to name the activities in the data set
-# 4. Appropriately labels the data set with descriptive variable names. 
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average 
-#    of each variable for each activity and each subject.
+# download data if not present in the current directory
+data.url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+data.fn <- "data.zip"
+if(!file.exists(data.fn)){
+  download.file(data.url, data.fn, method = "curl")
+}
+unzip(data.fn, overwrite = F)
 
+# input file paths
 data_dir      <- file.path("UCI HAR Dataset")
 test_set.fp   <- file.path(data_dir, "test", "X_test.txt")
 train_set.fp  <- file.path(data_dir, "train", "X_train.txt")
@@ -15,45 +17,30 @@ train_activities.fp <- file.path(data_dir, "train", "y_train.txt")
 test_sbj.fp  <- file.path(data_dir, "test", "subject_test.txt")
 train_sbj.fp <- file.path(data_dir, "train", "subject_train.txt")
 
+# 1. Merges the training and the test sets to create one data set.
+library(plyr)
+full_set <- ldply(c(test_set.fp, train_set.fp), function(x)read.table(x, header = F, colClasses = "numeric"))
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 library(dplyr)
+ms.features <- read.table(features.fp, stringsAsFactors = F, 
+                                 col.names = c("feature_id", "feature_description")) %>% 
+  filter(grepl(pattern = "mean\\(|std\\(", feature_description))
+full_set.s <- full_set[, ms.features$feature_id]
 
-## test
-test_set <- read.table(test_set.fp, header = F, colClasses = "numeric")
-## train
-train_set <- read.table(train_set.fp, header = F, colClasses = "numeric")
-
-## 1. merge sets and 2. extract mean and std
-features <- read.table(features.fp, stringsAsFactors = F, col.names = c("feature_id", "feature_description"))
-
-ms.features <- features[which(grepl(pattern = "mean\\(|std\\(", x = features$feature_description)),]
-
-full_set <- rbind(test_set, train_set)[, ms.features$feature_id]
-
-## 4. label the data set with descriptive variable names
-colnames(full_set) <- ms.features$feature_description
-
-# 3. Use descriptive activity names
+# 3. Uses descriptive activity names to name the activities in the data set
 activities <- read.table(activities.fp, col.names = c("activity_id", "activity_description"))
+r.activities <- ldply(c(test_activity.fp, train_activities.fp), function(x)read.table(x))
+dan <- activities[r.activities[,1], "activity_description"]
 
-test_activity <- read.table(test_activity.fp, header = F, colClasses = "numeric", col.names = c("activity_id"))
-test_activities <- merge(test_activity, activities, by = "activity_id", all.x = T, all.y = F)
-r.test_activities <- as.character(test_activities$activity_description)
+# 4. Appropriately labels the data set with descriptive variable names. 
+colnames(full_set.s) <- ms.features$feature_description
+full_set.s <- mutate(full_set.s, activity = dan)
 
-train_activity <- read.table(train_activities.fp, header = F, colClasses = "numeric", col.names = c("activity_id"))
-train_activities <- merge(train_activity, activities, by = "activity_id", all.x = T, all.y = F)
-r.train_activities <- as.character(train_activities$activity_description)
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average 
+#    of each variable for each activity and each subject.
+r.subjects <- ldply(c(test_sbj.fp, train_sbj.fp), function(x)read.table(x))
+itd <- mutate(full_set.s, subjects = r.subjects[,1])
 
-test_sbj <- scan(test_sbj.fp, what = "c")
-train_sbj <- scan(train_sbj.fp, what = "c")
-
-full_set <- mutate(full_set, activity = c(r.test_activities, r.train_activities),
-                   subjects = c(test_sbj, train_sbj))
-
-## 5. make tidy data and write it
-tidy_ds <- group_by(full_set, activity, subjects) %>% summarise_each(funs(mean, "mean", mean(., na.rm = TRUE)))
-# tidy_ds.act <- group_by(full_set[, colnames(full_set)!="subjects"], 
-#                         activity) %>% summarise_each(funs(mean, "mean", mean(., na.rm = TRUE)))
-# tidy_ds.sbj <- group_by(full_set[, colnames(full_set)!="activity"], 
-#                         subjects) %>% summarise_each(funs(mean, "mean", mean(., na.rm = TRUE)))
-
+tidy_ds <- group_by(itd, subjects, activity) %>% summarise_each(funs(mean, "mean", mean(., na.rm = TRUE)))
 write.table(tidy_ds, "tidyDS.txt", row.names = F)
